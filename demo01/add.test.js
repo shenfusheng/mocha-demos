731ee8c04b6a06f504e7430f20fd92e903296ce2
沈福生 <101203@nd.com>
2019-02-25 11:57:24 +0800

    update

--- demo01/add.test.js
+++ demo01/add.test.js
@@ -5,4 +5,7 @@ describe('加法函数的测试', function() {
   it('1 加 1 应该等于 2', function() {
     expect(add(1, 1)).to.be.equal(2);
   });
+  it('1 加 -1 应该等于 0', function() {
+    expect(add(1, -1)).to.be.equal(0);
+  });
 });
--- demo02/test/dir/multiply.test.js
+++ demo02/test/dir/multiply.test.js
@@ -5,4 +5,7 @@ describe('乘法函数的测试', function() {
   it('1 乘 1 应该等于 1', function() {
     expect(multiply(1, 1)).to.be.equal(1);
   });
+  it('任何数乘 0 应该等于 0', function() {
+    expect(multiply(10, 0)).to.be.equal(0);
+  });
 })
